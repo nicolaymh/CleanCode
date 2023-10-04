de9e8208141ba2e90e79659b5d2095f545ba4ea4
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-10-03 21:26:14 -0500

    solid - 04-segregation.ts: Principio de segregacion de interfaz

---
+++ src/solid/04-segregation.ts
@@ -0,0 +1,36 @@
+(() => {
+   // ISP: Principio de segregacion de inerfaz.
+
+   interface Bird {
+      eat(): void;
+   }
+   interface FlyingBird {
+      fly(): void;
+   }
+   interface RunningBird {
+      run(): void;
+   }
+   interface SwimmerBird {
+      swim(): void;
+   }
+
+   class Tucan implements Bird, FlyingBird {
+      public fly(): void {}
+      public eat(): void {}
+   }
+
+   class Hummingbird implements Bird, FlyingBird {
+      public fly(): void {}
+      public eat(): void {}
+   }
+
+   class Ostrich implements Bird, RunningBird {
+      public eat(): void {}
+      public run(): void {}
+   }
+
+   class Penguin implements Bird, SwimmerBird {
+      public eat(): void {}
+      public swim(): void {}
+   }
+})();
